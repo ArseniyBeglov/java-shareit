1783945b2e2ad6cac71dcc0d10f92900538c5194
adbeglov <HUDAIDOTT@yandex.ru>
2023-02-18 23:20:57 +0300

    sprint 16: some fixes after review

--- server/src/main/java/ru/practicum/shareit/user/UserController.java
+++ server/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,10 +1,7 @@
 package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.common.Create;
-import ru.practicum.shareit.common.Update;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
 
@@ -28,12 +25,12 @@ public class UserController {
     }
 
     @PostMapping
-    public UserDto create(@Validated(Create.class) @RequestBody UserDto userDto) {
+    public UserDto create(@RequestBody UserDto userDto) {
         return userService.create(userDto);
     }
 
     @PatchMapping("/{id}")
-    public UserDto patch(@PathVariable long id, @Validated(Update.class) @RequestBody UserDto userDto) {
+    public UserDto patch(@PathVariable long id, @RequestBody UserDto userDto) {
         return userService.update(id, userDto);
     }
 
